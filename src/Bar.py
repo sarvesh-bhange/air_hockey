01b2c0dd8a99f1105c2e942285025477706fc565
sarvesh-bhange <sarvesh.bhange@gmail.com>
2022-01-19 20:27:27 +0530

    border and collision done

--- src/Bar.py
+++ src/Bar.py
@@ -1,7 +1,6 @@
 import pygame
 from constant import *
 
-
 class Bar (object):
     def __init__(self, x, y, width, height, colour):
         self.x = x
@@ -11,7 +10,15 @@ class Bar (object):
         self.colour = colour
 
     def draw(self, surface):
+        print(self.y)
         bar_rect = pygame.Rect(self.x, self.y, self.width, self.height)
         pygame.draw.rect(surface, self.colour,bar_rect)
+        
+    def move(self,keys,up_key,down_key,wall,wall2):
+        if(keys[up_key] and self.y-vel  > BORDER_HEIGHT):
+            self.y-=vel
 
-                         
+        if(keys[down_key]and self.y+vel+BAR_HEIGHT<wall.y):
+            self.y+=vel
+
+                              
--- src/main.py
+++ src/main.py
@@ -11,13 +11,18 @@ white_bar=Bar(0,HEIGHT/2-BAR_HEIGHT/2,BAR_WIDTH,BAR_HEIGHT,WHITE)
 
 black_bar=Bar(WIDTH-BAR_WIDTH,HEIGHT/2-BAR_HEIGHT/2,BAR_WIDTH,BAR_HEIGHT,BLACK)
 
+border1=pygame.Rect(0,460,BORDER_WIDTH,BORDER_HEIGHT)
+
+border2=pygame.Rect(0,0,BORDER_WIDTH,BORDER_HEIGHT)
+
+
 def draw_window():
     win.fill(PURPLE,(0,0,WIDTH,HEIGHT))
     white_bar.draw(win)
     black_bar.draw(win)
+    pygame.draw.rect(win,(BLACK),border1)
+    pygame.draw.rect(win,(BLACK),border2)
     pygame.display.update()
-
-
 
 # Main Loop
 
@@ -28,5 +33,11 @@ while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
-            pygame.quit
+
+    keys=pygame.key.get_pressed()
+    
+    black_bar.move(keys,pygame.K_UP, pygame.K_DOWN,border1,border2)
+    white_bar.move(keys,pygame.K_w,pygame.K_s,border1,border2)
+    
     draw_window()
+    pygame.quit
